3a618832920914cf687003a621fe69ecf5e231a4
Andy Ernst <andycernst@gmail.com>
2014-09-13 08:25:56 -0700

    click to add feature working on side-by-side view

---
+++ data/data.js
@@ -0,0 +1,11 @@
+var data = angular.module("data", []);
+
+data.value(
+  "paragraphs", [
+    "I come from a liberal arts background where I developed a habit of learning difficult things quickly. In two years in Japan I went from completely overwhelmed to a confident teacher of hundreds of students and a fluent speaker of Japanese. After returning from Japan I discovered my true passion, programming. That passion drives me today to improve on what I know, learn from others, and one day build a great web app that changes people lives.",
+
+    "I bring much more to the table than just programming skill -- I know how to train others, how to communicate on a professional level, and how to cut through clutter to get a job done quickly. At Lionbridge I was recognized as one of the best employees in an office of over 50 people. I know I can do the same at your company."
+  ]
+).value(
+  "otherthing", "thing"
+);
--- js/controllers.js
+++ js/controllers.js
@@ -8,9 +8,31 @@ var snippets = [
 	{name: "html", info: "I know html"}
 ];
 
-coverLetterApp.controller("CoverBuilderController", function($scope, $http){
-	$scope.snippets = snippets;
-	$http.get("data/data.json").success(function(data){
-		$scope.firstParagraph = data;
-	});
-});
+coverLetterApp.controller("CoverBuilderController", ["$scope", "$http",
+  function($scope, $http) {
+    $scope.snippets = snippets;
+    $scope.bodyParagraphs = paragraphs.body;
+    $scope.endingParagraphs = paragraphs.ending;
+
+  }
+]);
+
+var paragraphs = { 
+	body:[
+	{
+    text: "I come from a liberal arts background where I developed a habit of learning difficult things quickly. In two years in Japan I went from completely overwhelmed to a confident teacher of hundreds of students and a fluent speaker of Japanese. After returning from Japan I discovered my true passion, programming. That passion drives me today to improve on what I know, learn from others, and one day build a great web app that changes people lives."
+  },
+
+  {
+    text: "I bring much more to the table than just programming skill -- I know how to train others, how to communicate on a professional level, and how to cut through clutter to get a job done quickly. At Lionbridge I was recognized as one of the best employees in an office of over 50 people. I know I can do the same at your company."
+  }],
+  ending:[
+  {
+  	text: "Let’s do great things together."
+  },
+  {
+  	text: "I look forward to hearing from you."
+  }
+  ]
+};
+
