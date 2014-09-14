1c16891c2501a903a314e7e5af1f925c806957fe
Andy Ernst <andycernst@gmail.com>
2014-09-14 14:22:45 -0700

    refactor controler data and logic to be clearer

--- js/controllers.js
+++ js/controllers.js
@@ -5,52 +5,65 @@ var coverLetterApp = angular.module("coverLetterApp", []);
 
 coverLetterApp.controller("CoverBuilderController", ["$scope", "$http",
   function($scope, $http) {
-    $scope.snippets = snippets;
-    paragraphs.body.forEach(function(paragraph){
-    	paragraph.sentences = splitAndObjectfy(paragraph.text);
-    })
-		
-    $scope.bodyParagraphs = paragraphs.body;
-    $scope.endingParagraphs = paragraphs.ending;
+
+    textData.bodyParagraphs.forEach(function(paragraph) {
+        paragraph.sentences = splitAndObjectfy(paragraph.text);
+      });
+    console.log(textData.bodyParagraphs);
+    $scope.bodyParagraphs = textData.bodyParagraphs;
+
+    $scope.snippets = textData.snippets;
+    $scope.endingParagraphs = textData.endingParagraphs;
   }
 ]);
 
-var splitAndObjectfy = function(text){
-	var sentences = text.match(/[^\.!\?]+[\.!\?\s]+/g);
+// This splits a paragraph into an array of sentence objects 
+// with the key "text" = the sentence string.
+var splitAndObjectfy = function(text) {
+  var sentences = text.match(/[^\.!\?]+[\.!\?\s]+/g);
 
-	return sentences.map(function(sentence){
-		return {text: sentence};
-	});
+  return sentences.map(function(sentence) {
+    return {
+      text: sentence
+    };
+  });
 };
 
-var paragraphs = { 
-	body:[
-	{
-    text: "I come from a liberal arts background where I developed a habit of learning difficult things quickly. In two years in Japan I went from completely overwhelmed to a confident teacher of hundreds of students and a fluent speaker of Japanese. After returning from Japan I discovered my true passion, programming. That passion drives me today to improve on what I know, learn from others, and one day build a great web app that changes people lives."
-  },
+var textData = {
 
-  {
+  bodyParagraphs: [{
+    text: "In my liberal arts education I developed the skill of learning difficult things quickly. In three years in Japan I went from completely overwhelmed to a confident teacher of hundreds of students and a proficient speaker of Japanese. After returning from Japan I discovered my true passion, programming. That passion drives me today to improve on what I know, learn from others, and one day build a great web app that changes people lives."
+  }, {
     text: "I bring much more to the table than just programming skill -- I know how to train others, how to communicate on a professional level, and how to cut through clutter to get a job done quickly. At Lionbridge I was recognized as one of the best employees in an office of over 50 people. I know I can do the same at your company."
   }],
-  ending:[
-  {
-  	text: "I am very excited to learn more about this opportunity and share how I will be a great fit for this position."
-  },
-   {
-  	text: "I would appreciate the opportunity to meet with you to discuss how my qualifications will be beneficial to your company's success."
-  },
-  {
-  	text: "I look forward to hearing from you."
-  }
+
+  endingParagraphs: [{
+    text: "I am very excited to learn more about this opportunity and share how I will be a great fit for this position."
+  }, {
+    text: "I would appreciate the opportunity to meet with you to discuss how my qualifications will be beneficial to your company's success."
+  }, {
+    text: "I look forward to hearing from you."
+  }],
+
+  snippets: [{
+      name: "javascript",
+      info: "Used full-stack, OO JavaScript (including Angular, Backbone, Node, Handlebars, jQuery, AJAX) to write complex web applications and interactive websites"
+    },
+
+    {
+      name: "responsive design",
+      info: "Currently designing mobile-first, responsive websites with HTML5/CSS3"
+    },
+
+    {
+      name: "design",
+      info: "Keen eye for the subtle things that make a great user experience"
+    },
+
+    {
+      name: "java",
+      info: "Developed a Java algorithm to find idea areas for investment from 1+ million lines of unemployment data."
+    }
   ]
+
 };
-
-var snippets = [
-	{name: "javascript", info: "Used full-stack, OO JavaScript (including Angular, Backbone, Node, Handlebars, jQuery, AJAX) to write complex web applications and interactive websites"},
-
-	{name: "responsive design", info: "Currently designing mobile-first, responsive websites with HTML5/CSS3"},
-
-	{name: "design", info: "Keen eye for the subtle things that make a great user experience"},
-
-	{name: "java", info: "Developed a Java algorithm to find idea areas for investment from 1+ million lines of unemployment data."}
-];
